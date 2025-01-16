a4692563e56bbec58c4daf680b22acab1d607039
Florian Moser <florian98.moser@outlook.de>
2025-01-16 16:44:20 +0100

    input to js

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -1,7 +1,11 @@
 const core = require('@actions/core')
 
 function run() {
+    const bucket = core.getInput('bucket', { required: true})
+    const distFolder = core.getInput('dist-folder', { required: true})
     core.notice('Hello World');
+    core.notice(bucket);
+    core.notice(distFolder);
 }
 
 run();
